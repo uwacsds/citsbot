7396a7a7ed56a856f9a58707df1e6bd93d5f5b21
Tim <tim@tim-ings.com>
2020-03-31 21:21:41 +0800

    Black precommit hook test

--- src/modules/announcer.py
+++ src/modules/announcer.py
@@ -13,7 +13,10 @@ class Announcer(commands.Cog):
     async def on_ready(self):
         self.announce_channel = self.bot.get_channel(self.cfg.modules.announcer.channel)
         self.scheduler = AsyncIOScheduler()
-        self.scheduler.add_job(self.announce_week, trigger=CronTrigger.from_crontab(self.cfg.modules.announcer.crontab))
+        self.scheduler.add_job(
+            self.announce_week,
+            trigger=CronTrigger.from_crontab(self.cfg.modules.announcer.crontab),
+        )
         self.scheduler.start()
 
     @commands.command()
@@ -22,16 +25,18 @@ class Announcer(commands.Cog):
 
     async def announce_week(self):
         events = [
-            { 'title': 'CITS1001', 'content': 'Lab 8 is due this week' },
-            { 'title': 'CITS3402', 'content': 'Assignment 2 is due this week' },
-            { 'title': 'STAT2401', 'content': 'Exam is this week' },
+            {"title": "CITS1001", "content": "Lab 8 is due this week"},
+            {"title": "CITS3402", "content": "Assignment 2 is due this week"},
+            {"title": "STAT2401", "content": "Exam is this week"},
         ]
 
-        title = 'Welcome to Week 9 of Semester 2'
-        desc = 'Here are some things happening this week'
+        title = "Welcome to Week 9 of Semester 2"
+        desc = "Here are some things happening this week"
 
-        emb = discord.Embed(title=title, description=desc, colour=discord.Colour.from_rgb(8, 100, 165))
+        emb = discord.Embed(
+            title=title, description=desc, colour=discord.Colour.from_rgb(8, 100, 165)
+        )
         for e in events:
-            emb.add_field(name=e['title'], value=e['content'])
-        
+            emb.add_field(name=e["title"], value=e["content"])
+
         await self.announce_channel.send(embed=emb)
