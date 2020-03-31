d3d0345ec24f2e1e6dbdbd9cbd176752ecae5bb3
M Nef <howtoeatpineapples@gmail.com>
2020-04-01 00:13:41 +0800

    add and remove commnads for unit roles

--- src/modules/message_roles.py
+++ src/modules/message_roles.py
@@ -6,3 +6,13 @@ class MessageRoles(commands.Cog):
     def __init__(self, bot, cfg):
         self.bot = bot
         self.cfg = cfg
+
+    @commands.command()
+    async def add(self, ctx, *, txt):
+        # TODO: allow users to add roles with !add
+        ...
+
+    @commands.command()
+    async def remove(self, ctx, *, txt):
+        # TODO: allow users to remove roles added by !add
+        ...
