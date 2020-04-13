ef98aeb6cff15614d701ef6dedf17881fa332b4a
Tim <tim@tim-ings.com>
2020-04-13 23:22:06 +0800

    Improves run script

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t citsbot . && docker run -it -v src:/app/src --env-file .env citsbot
+docker build -t citsbot . \ # build the image
+&& docker run --name citsbot -it -v src:/app/src --env-file .env citsbot \ # create and run a new container from the image
+|| docker restart citsbot && docker container attach citsbot \ # if we already have a continer, restart and attach to it
+; docker stop citsbot # finally always stop the container
