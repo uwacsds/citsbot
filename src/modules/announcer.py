8e698cd506aba1cd50b0290b8f7624f0af71bfa2
Tim <tim@tim-ings.com>
2020-04-19 22:45:55 +0800

    Changes warning symbol in announcer footer from image to emoji

--- src/modules/announcer.py
+++ src/modules/announcer.py
@@ -72,10 +72,7 @@ class Announcer(commands.Cog):
 
         emb = discord.Embed(title=title, description=desc, colour=discord.Colour.blue())
         emb.set_image(url="https://i.imgur.com/2cQttpX.png")
-        emb.set_footer(
-            text="WARNING: Unit deadlines are NOT guaranteed to be listed here",
-            icon_url="https://i.imgur.com/q7OZqHY.png",
-        )
+        emb.set_footer(text="⚠️ WARNING: Unit deadlines are NOT guaranteed to be listed here")
         for e in events:
             emb.add_field(name=f":pencil: {e['title']}", value=e["content"], inline=False)
 
