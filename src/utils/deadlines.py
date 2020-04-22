4129f5eb219aea2167487f6b95368fae3a4b690f
hvislyset <kmcdonal96@gmail.com>
2020-04-22 23:58:02 +0800

    Ignore postgrad deadlines by default

--- src/utils/deadlines.py
+++ src/utils/deadlines.py
@@ -76,14 +76,19 @@ class Deadlines:
         """
         return start <= due_date <= end
 
-    def get_deadlines_this_week(self, date) -> list:
+    def get_deadlines_this_week(self, date, include_postgrad=False) -> list:
         """
-        Gets the deadlines occurring in the week that date belongs to
+        Gets the deadlines occurring in the week that date belongs to. Ignore postgrad units by default
         """
         monday = date - timedelta(days=date.weekday())
         for unit in self.units:
+            # Skip over any postgrad units in the range CITS(456+)xxx unless otherwise specified
+            if not include_postgrad and unit["title"][4] in [str(x) for x in range(4, 10)]:
+                print("Ignoring postgrad unit:", unit["title"])
+                continue
             for assignment in unit["assignments"]:
-                print("Checking:", unit["title"], "assignment:", assignment["title"])
+                print("Checking:", unit["title"],
+                      "assignment:", assignment["title"])
                 if self.__is_due_this_week(monday, monday + timedelta(days=7), assignment["due_date"]):
                     print("\tAssignment is in range")
                     self.deadlines.append(
