8b8f09966d0dad9c1c3cc99410c42019137b3fa5
Tim Ings <tim@tim-ings.com>
2020-05-15 23:39:58 +0800

    Fixes issues in PR

--- src/modules/stack_overflow.py
+++ src/modules/stack_overflow.py
@@ -1,80 +1,82 @@
+import os
+import aiohttp
+from bs4 import BeautifulSoup
 import discord.ext.commands as commands 
 import discord.embeds as embed
 import discord.colour as colour
-import requests, stackexchange 
-from logger import ErrorLevel
-from bs4 import BeautifulSoup
 
-''' ISSUES: 
-1) beautiful soup removes all links (<a href>) in answer_body
-2) include question body as well?  
-'''
+
+BODY_MAX_LEN = 2048
+
+async def fetch_json(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            return await resp.json()
+
+def mark_up(html):
+    return html.replace("<pre><code>","```").replace("</code></pre>","```")
+
+class StackOverflowConfig():
+    def __init__(self, cfg):
+        self.google_search_id = os.getenv("GOOGLE_SEARCH_ID")
+        self.google_search_key = os.getenv("GOOGLE_SEARCH_KEY")
+        self.stack_overflow_key = os.getenv("STACK_OVERFLOW_KEY") or ""
+
 class StackOverflow(commands.Cog):
     def __init__(self, bot, cfg, logger):
         self.bot = bot
         self.logger = logger
-        mod = cfg.modules.stack_overflow
-        self.engine_id = mod.googlesearch_engine_id
-        self.key = mod.googlesearch_key
+        self.cfg = StackOverflowConfig(cfg)
 
-    # uses Discord's ```  ``` code block markdown
-    def markUp(self, str):
-        return str.replace("<pre><code>","```").replace("</code></pre>","```")
-            
-    ''' Uses google search API first because Stack Overflow API can only return questions that exactly match the search terms'''
-    def findAnswer(self, search_term):
+    async def find_question(self, search_terms):
+        search_query = '%20'.join(search_terms.split(" "))
+        url = f"https://www.googleapis.com/customsearch/v1/siterestrict?q={search_query}&cx={self.cfg.google_search_id}&key={self.cfg.google_search_key}"
+        return (await fetch_json(url))["items"][0]['link']
+
+    async def fetch_question_title(self, question_url):
+        question_id = int(question_url.split("/")[4])
+        url = f"https://api.stackexchange.com/2.2/questions/{question_id}?site=stackoverflow"
+        return (await fetch_json(url))["items"][0]["title"]
+
+    async def fetch_answers(self, question_url):
+        question_id = int(question_url.split("/")[4])
+        url = f"https://api.stackexchange.com/2.2/questions/{question_id}/answers?order=desc&sort=votes&site=stackoverflow&filter=withbody&key={self.cfg.stack_overflow_key}"
+        return await fetch_json(url)
+
+    def find_best_answer(self, question_json):
+        top_rated = None
+        top_score = -99999
+        for answer in question_json["items"]:
+            if answer["is_accepted"]:
+                return answer
+            if answer["score"] > top_score:
+                top_score = answer["score"]
+                top_rated = answer
+        return top_rated
+
+    def format_answer_body(self, answer):
+        answer_html = mark_up(answer["body"])
+        soup = BeautifulSoup(markup=answer_html, features="html.parser")
+        answer_body = soup.get_text()
+        if len(answer_body) > BODY_MAX_LEN:
+            answer_body = answer_body[:(BODY_MAX_LEN-3)] + "..."
+        return answer_body
+
+    @commands.command(name="so")
+    async def stack_overflow(self, ctx, *, search_terms):
         try:
-            search_term = '%20'.join(search_term.split(" "))
+            question_url = await self.find_question(search_terms)
+            question_title = await self.fetch_question_title(question_url)
+            answers = await self.fetch_answers(question_url)
+            best_answer = self.find_best_answer(answers)
+            answer_body = self.format_answer_body(best_answer)
 
-            r = requests.get("https://www.googleapis.com/customsearch/v1/siterestrict?q=" + search_term + "&cx=" + self.engine_id + "&key=" + self.key)
-            json_content = r.json()
-            question_url = json_content["items"][0]['link']
-            id = int(question_url.split("/")[4])
-            so = stackexchange.Site(stackexchange.StackOverflow)
-            question = so.question(id, body=True)
-            question_title = "Q: " + question.title
-            question_body = question.body
-            #question_body = self.markUp(BeautifulSoup(question.body).get_txt())
-            answers = question.answers
-            answer_body = ""
-            for answer in answers:
-                if (answer.accepted == True):
-                    answer_body = answer.body
-
-            # no accepted answer, grab highest voted one instead 
-            if (answer_body == ""):
-                answers.sort(key = lambda answer: answer.score, reverse=True)
-                answer_body = answers[0].body
-
-            answer_body = self.markUp(answer_body)
-            soup = BeautifulSoup(markup=answer_body,features="html.parser")
-            answer_body = soup.get_text()
-            answer_body = "Top answer: " + answer_body 
-            if (len(answer_body) > 2048):
-                answer_body = answer_body[:2045] + "..."
-                # max length is 2048
-
-
-            return question_title, question_body, answer_body, question_url
-
-        except NameError:
-            return -1
-
+            embedded_msg = embed.Embed(title=f"Q: {question_title}", 
+                                        url=question_url, 
+                                        description=answer_body,  
+                                        colour=colour.Colour.from_rgb(244, 128, 36))
+            embedded_msg.set_author(name="Stack Overflow", icon_url="https://i.imgur.com/d6LbAzp.png")
+            await ctx.channel.send(embed=embedded_msg)
         except:
-            return -2
-
-    @commands.command()
-    async def stackOverflow(self, ctx, *, txt):
-        content = self.findAnswer(txt)
-        if (content == -2):
-            await ctx.channel.send("Those search terms are too broad")
-        elif (content == -1): 
-            await ctx.channel.send("No Stack Overflow forum could be found with those search terms")
-        else:
-            embedded_msg = embed.Embed(title=content[0], description=content[2], colour=colour.Colour.from_rgb(222,148,10))
-            embedded_msg.set_author(name="Stack Overflow")
-            embedded_msg.set_footer(text="Read full forum: " + content[-1])
-
-            await ctx.channel.send(content=None, embed=embedded_msg)
+            await ctx.channel.send("Failed to find a stack overflow question matching those terms")
          
-''' update search engine ID''' 
