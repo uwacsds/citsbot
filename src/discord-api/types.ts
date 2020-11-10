504b931987c01c28972d2be0f73700d849869c9c
Tim Ings <tim@tim-ings.com>
2020-11-10 18:24:50 +0800

    Adds better env variable parsing

--- src/discord-api/types.ts
+++ src/discord-api/types.ts
@@ -1,6 +1,6 @@
 export type MessageTuple = [channelId: string, messageId: string];
 
 export interface DiscordAPI {
-  start: () => Promise<void>;
+  start: (discordToken: string) => Promise<void>;
   stop: () => Promise<void>;
 }
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,23 @@ import { BotConfig, loadConfig } from './domain/config';
 import { MessageTuple } from './discord-api/types';
 import { academicCalendarService } from './academic-calendar/academic-calendar-service';
 
+const env = {
+  CONFIG: process.env.CONFIG as string,
+  DISCORD_ID: process.env.DISCORD_ID as string,
+  DISCORD_SECRET: process.env.DISCORD_SECRET as string,
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN as string,
+};
+
 const getMessagesToCache = (config: BotConfig): MessageTuple[] => [
   ...config.modules.reactRoles.messages.map((msg): MessageTuple => [msg.channel, msg.id]),
 ];
 
 const start = async () => {
-  const config = await loadConfig('./config.json');
+  const config = await loadConfig(env.CONFIG);
   const calendar = academicCalendarService();
   const commandHandler = discordCommandHandler(config, calendar);
   const { start } = discordApi(commandHandler, getMessagesToCache(config));
-  await start();
+  await start(env.DISCORD_TOKEN);
 };
 
 start();
