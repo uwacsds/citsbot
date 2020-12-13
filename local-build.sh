5a9f9fe1e2c868e8125e407e9d5b7c7c0e5ded5d
Tim Ings <tim@tim-ings.com>
2020-12-13 16:34:21 +0800

    Fixes build script

--- local-build.sh
+++ local-build.sh
@@ -6,6 +6,6 @@ VERSION=$(cat ./helm/citsbot/Chart.yaml | grep version | awk '{ print $2 }')
 docker build -t citsbot:${VERSION} . --target dev
 
 # minikube uses a separate daemon to the host, so images built on the host need to be transferred to it
-if [[ "$LOCAL_KUBE_ENV" = "minikube" ]]; then
+if [[ "$LOCAL_KUBE_CONTEXT" = "minikube" ]]; then
   docker save citsbot:${VERSION} | (eval $(minikube docker-env) && docker load)
 fi
