4876f2bd1bfbf1076273b3fc4211c3356af4db9b
Tim Ings <tim@tim-ings.com>
2020-12-13 16:30:52 +0800

    Adds minikube copy step to build.sh

--- local-build.sh
+++ local-build.sh
@@ -4,3 +4,8 @@ sh ./local-set-context.sh
 
 VERSION=$(cat ./helm/citsbot/Chart.yaml | grep version | awk '{ print $2 }')
 docker build -t citsbot:${VERSION} . --target dev
+
+# minikube uses a separate daemon to the host, so images built on the host need to be transferred to it
+if [[ "$LOCAL_KUBE_ENV" = "minikube" ]]; then
+  docker save citsbot:${VERSION} | (eval $(minikube docker-env) && docker load)
+fi
