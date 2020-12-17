0ae2d1bac45490dbbf458f78d724a6c696359c9c
Tim Ings <tim@tim-ings.com>
2020-12-17 15:57:03 +0800

    Observability (#47)
    * Adds basic discord metrics
    
    * Adds pushgateway url to prod and stage
    
    * Runs linter

---
+++ src/metrics/process.ts
@@ -0,0 +1,10 @@
+import { Pushgateway } from 'prom-client';
+
+export const initPushgateway = (environment: string, url: string | undefined) => {
+  if (!url) {
+    console.log(`No pushgateway URL provided for ${environment}. No metrics will be pushed.`);
+    return setInterval(() => {}, 5000);
+  }
+  const pushgateway = new Pushgateway(url);
+  return setInterval(() => pushgateway.pushAdd({ jobName: `citsbot-${environment}` }, () => {}), 5000);
+};
