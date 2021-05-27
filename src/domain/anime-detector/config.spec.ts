ebc23f49692d2cec438a81b30e3ce15b7540ab72
Tim Ings <tim@tim-ings.com>
2021-05-28 01:04:32 +0800

    Fix anime detector config validator messages

--- src/domain/anime-detector/config.spec.ts
+++ src/domain/anime-detector/config.spec.ts
@@ -1,6 +1,6 @@
 import { AnimeDetectorConfig, validateAnimeDetectorConfig } from './config';
 
-describe(`cowsay config`, () => {
+describe(`anime detector config`, () => {
   const validConfig: AnimeDetectorConfig = {
     keywords: [`keyword-123`, `keyword-234`, `keyword-345`],
     keywordCountThreshold: 5,
@@ -12,16 +12,16 @@ describe(`cowsay config`, () => {
 
   test(`given invalid keywordCountThreshold > when validate > should throw`, () => {
     const rawConfig = { ...validConfig, keywordCountThreshold: undefined };
-    expect(() => validateAnimeDetectorConfig(rawConfig)).toThrowError(`Config Validation: modules.cowsay: Expected 'keywordCountThreshold' to have type 'number', got 'undefined'`);
+    expect(() => validateAnimeDetectorConfig(rawConfig)).toThrowError(`Config Validation: modules.animeDetector: Expected 'keywordCountThreshold' to have type 'number', got 'undefined'`);
   });
 
   test(`given invalid keywords > when validate > should throw`, () => {
     const rawConfig = { ...validConfig, keywords: undefined };
-    expect(() => validateAnimeDetectorConfig(rawConfig)).toThrowError(`Config Validation: modules.cowsay: Expected 'keywords' to have type 'array', got 'undefined'`);
+    expect(() => validateAnimeDetectorConfig(rawConfig)).toThrowError(`Config Validation: modules.animeDetector: Expected 'keywords' to have type 'array', got 'undefined'`);
   });
 
   test(`given invalid keyword type > when validate > should throw`, () => {
     const rawConfig = { ...validConfig, keywords: [...validConfig.keywords, 123] };
-    expect(() => validateAnimeDetectorConfig(rawConfig)).toThrowError(`Config Validation: modules.cowsay.keywords[3]: Expected element to have type 'string', got 'number'`);
+    expect(() => validateAnimeDetectorConfig(rawConfig)).toThrowError(`Config Validation: modules.animeDetector.keywords[3]: Expected element to have type 'string', got 'number'`);
   });
 });
--- src/domain/anime-detector/config.ts
+++ src/domain/anime-detector/config.ts
@@ -6,14 +6,14 @@ export type AnimeDetectorConfig = {
 }
 
 export const validateAnimeDetectorConfig = (rawConfig: Record<string, unknown>): AnimeDetectorConfig => {
-  const { validateValue, validateArray } = fieldValidator(`Config Validation: modules.cowsay`);
+  const { validateValue, validateArray } = fieldValidator(`Config Validation: modules.animeDetector`);
   validateArray(rawConfig, `keywords`, validateKeyWord);
   validateValue(rawConfig, `keywordCountThreshold`, `number`);
   return rawConfig as AnimeDetectorConfig;
 };
 
 const validateKeyWord = (keyword: unknown, keywordIdx: number): keyword is string => {
-  const prefix = `Config Validation: modules.cowsay.keywords[${keywordIdx}]`;
+  const prefix = `Config Validation: modules.animeDetector.keywords[${keywordIdx}]`;
   if (!validString(keyword)) throw Error(`${prefix}: Expected element to have type 'string', got '${typeof keyword}'`);
   return true;
 };
