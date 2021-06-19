937a491951916b43c8b387d1a98ac454183fbc7f
Tim Ings <tim@tim-ings.com>
2021-06-19 18:31:40 +0800

    Adds upload image service

---
+++ src/domain/anime-detector/upload-image-service.ts
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+
+export interface ImageUploaderService {
+  (imageUrl: string): Promise<string>
+}
+
+export const imgurImageUploaderService = (
+  clientId: string,
+): ImageUploaderService =>
+  async imageUrl => {
+    const response = await fetch(`https://api.imgur.com/3/image`, {
+      method: `POST`,
+      headers: { 'Accept': `application/json`, 'Authorization': `Client-ID ${clientId}` },
+      body: imageUrl,
+    });
+
+    if (!response.ok) throw Error(`Failed to upload image to imgur. Got non-200 response code: ${response.status}, ${await response.text()}`);
+
+    const data = await response.json();
+    const imgurLink = data?.data?.link;
+    if (imgurLink === undefined) throw Error(`Failed to upload image to imgur. Link not present in response body: ${JSON.stringify(data)}`);
+
+    return imgurLink;
+  };
