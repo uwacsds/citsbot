e95577d99e8cd887149e35306217c4572c223017
Tim Ings <tim@tim-ings.com>
2022-03-26 21:12:58 +0800

    Upgrade to node 16

--- src/metrics/process.ts
+++ src/metrics/process.ts
@@ -6,5 +6,5 @@ export const initPushgateway = (url: string | undefined) => {
     return setInterval(() => undefined, 5000);
   }
   const pushgateway = new Pushgateway(url);
-  return setInterval(() => pushgateway.pushAdd({ jobName: `citsbot` }, () => undefined), 5000);
+  return setInterval(() => pushgateway.pushAdd({ jobName: `citsbot` }), 5000);
 };
